298ee9d71f1a1e2718d7a4f5a3d64e0e140d924c
shantiyoga <shantiyogars@gmail.com>
2023-09-13 11:46:22 +0700

    Update: README.md

--- main/views.py
+++ main/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_item(request):
     context = {
+        'creator' : 'Shanti Yoga Rahayu',
+        'id' : 2206082360,
+        'class' : 'PBP D',
         'name': 'Peony',
         'amount': 52,
         'description': 'The peony is a lush and extravagant flowering plant known for its large, fragrant, and beautifully layered blooms in a variety of colors, making it a symbol of romance and prosperity.',
