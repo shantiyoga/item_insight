a2936c7b8ade59022315a0a1c4eeec48f9a38a9a
shantiyoga <shantiyogars@gmail.com>
2023-09-13 10:45:18 +0700

    Update README.md

--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from main.models import ItemStore
 
 # Create your tests here.
 class mainTest(TestCase):
@@ -9,3 +10,17 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+        
+    def test_items_store_creation(self):
+        item = ItemStore.objects.create(
+            name= 'Peony',
+            amount= 52,
+            description= 'The peony is a lush and extravagant flowering plant known for its large, fragrant, and beautifully layered blooms in a variety of colors, making it a symbol of romance and prosperity.',
+            price= 57000,
+            category='Flower',
+        )
+        self.assertEqual(item.name, 'Peony')
+        self.assertEqual(item.amount, 52)
+        self.assertEqual(item.description, 'The peony is a lush and extravagant flowering plant known for its large, fragrant, and beautifully layered blooms in a variety of colors, making it a symbol of romance and prosperity.',)
+        self.assertEqual(item.price, 57000)
+        self.assertEqual(item.category, 'Flower')
